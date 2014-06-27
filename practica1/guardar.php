adfab5d26827f1df66d585262f4ac909c59347ca
25AngelTorres <25AngelTorres@gmail.com>
2014-06-27 16:20:58 -0500

    primer commint

---
+++ practica1/guardar.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Lista de Equipos Brasil 2014</title>
+<link rel="stylesheet" type="text/css" href="CSS/estilos.css" />
+</head>
+
+<body style="background-image:url(Imagen/Fondo02.jpg); background-size:100%; ">  
+<?php
+echo "Hola...<br />";
+echo "Goles Equipo Local Holanda: ".$_POST['equipo_local_1'];
+echo "<br />";
+echo "Goles Equipo Visitante Espa&ntilde;a: ".$_POST['equipo_visitante_2'];
+echo "<br />";
+echo "Goles Equipo Local Croacia: ".$_POST['equipo_local_3'];
+echo "<br />";
+echo "Goles Equipo Visitante M&eacute;xico: ".$_POST['equipo_visitante_4'];
+echo "<br />";
+echo "Goles Equipo Local ".$_POST['equipo_local']." :".$_POST['equipo_local_5'];
+echo "<br />";
+echo "Goles Equipo Local ".$_POST['equipo_visitante']." :".$_POST['equipo_visitante_6'];
+?>
+</body>
+</html>
